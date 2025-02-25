eb6079567057cebc42d9fe55ed1b7e5558faae8e
yug49 <148035793+yug49@users.noreply.github.com>
2025-02-25 16:00:48 +0530

    simple storage done

---
+++ script/DeploySimpleStorage.s.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import "forge-std/Script.sol";
+import {SimpleStorage} from "../src/simpleStorage.sol";
+
+contract DeploySimpleStorage is Script {
+    function run() external returns(SimpleStorage) {
+        vm.startBroadcast(); //vm cheatcodes only valid in foundry and will not work on other solidity frameworks
+        SimpleStorage simpleStorage = new SimpleStorage();
+        vm.stopBroadcast();
+
+        return simpleStorage;
+    }
+}
--- src/simpleStorage.sol
+++ src/simpleStorage.sol
@@ -1,15 +1,16 @@
- // SPDX-License-Identifier: MIT
-pragma solidity 0.8.24; //solidity version
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18; //solidity version
+
 //>=0.8.18 <0.9.0; ---> for range of versions
 
 contract SimpleStorage {
     // Basic Types: boolean, unit, unit, int, address, bytes
     bool hasFavoriteNumber = true;
-    uint256 favoriteNumber = 88;    // default values like in java, int ---> 0, bool ---> false
+    uint256 favoriteNumber = 88; // default values like in java, int ---> 0, bool ---> false
     string text = "eighty-eight";
     // address myAddress = 0x55816e65FFFc44aB44f70Fb0986d72CE2C096003;
-    bytes32 public favoriteBytes32 = "cat";    // onny in this case bytes and bytes32 are different thing
-    uint256[] listOfFavoriteNumbers; 
+    bytes32 public favoriteBytes32 = "cat"; // onny in this case bytes and bytes32 are different thing
+    uint256[] listOfFavoriteNumbers;
 
     struct Person {
         uint256 favNo;
@@ -21,19 +22,18 @@ contract SimpleStorage {
     //     Name: "Pat"
     // });
 
-    Person[] public listOfPeople;   // dynamic array ---> size can be modified || if [3], then max of size 3 (static array)
+    Person[] public listOfPeople; // dynamic array ---> size can be modified || if [3], then max of size 3 (static array)
 
     mapping(string => uint256) public nameToFavNo;
 
     //default visibility is --> internal
 
-    function store(uint256 _favoriteNumber) public virtual  {
+    function store(uint256 _favoriteNumber) public virtual {
         favoriteNumber = _favoriteNumber;
     }
 
-
     //view, pure ---> specifying it is read operation and have no txn so, minimal gas
-    function retrieve() public view returns(uint256){
+    function retrieve() public view returns (uint256) {
         return favoriteNumber;
     }
 
@@ -45,7 +45,7 @@ contract SimpleStorage {
         // Person memory newPerson = Person(_favNo, _name);
         // listOfPeople.push(newPerson);
 
-        listOfPeople.push(Person(_favNo, _name));  
-        nameToFavNo[_name] = _favNo;                      
+        listOfPeople.push(Person(_favNo, _name));
+        nameToFavNo[_name] = _favNo;
     }
 }
