3fd6fdd319a0290b5191def96f1db340deac27cf
yug49 <148035793+yug49@users.noreply.github.com>
2025-03-29 03:23:31 +0530

    forge fmt

--- script/DeploySimpleStorage.s.sol
+++ script/DeploySimpleStorage.s.sol
@@ -6,7 +6,7 @@ import "forge-std/Script.sol";
 import {SimpleStorage} from "../src/simpleStorage.sol";
 
 contract DeploySimpleStorage is Script {
-    function run() external returns(SimpleStorage) {
+    function run() external returns (SimpleStorage) {
         vm.startBroadcast(); //vm cheatcodes only valid in foundry and will not work on other solidity frameworks
         SimpleStorage simpleStorage = new SimpleStorage();
         vm.stopBroadcast();
